e5ae42c492ed8a3f0b0d492760093e9784234c0f
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-06-27 13:52:45 -0400

    Add flake colors

--- project/src/FoodFlake.gd
+++ project/src/FoodFlake.gd
@@ -4,9 +4,18 @@ class_name FoodFlake
 export var fall_speed := 50
 
 onready var CollisionShape2D := $CollisionShape2D
+onready var sprite : AnimatedSprite = $AnimatedSprite
+
+func _ready():
+	randomize()
+	var number = randi()%3
+	if number == 1:
+		sprite.play("Pink")
+	elif number == 2:
+		sprite.play("Green")
+	elif number == 3:
+		sprite.play("Purple")
+
 
 func _process(delta):
 	position += Vector2(0,fall_speed * delta)
-
-func _draw():
-	draw_circle(Vector2.ZERO, CollisionShape2D.shape.radius, Color.beige)
