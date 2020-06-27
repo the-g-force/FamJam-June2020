54f7fe893fbb646bc2ff395a74604d588d66c7f6
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-06-27 09:55:02 -0400

    Add 'eye' to placeholder fish

--- project/src/Fish.gd
+++ project/src/Fish.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-
 func _draw():
 	var extents : Vector2 = $CollisionShape2D.shape.extents
 	var r : Rect2 = Rect2(-extents.x, -extents.y, extents.x*2, extents.y*2)
 	draw_rect(r, Color.red)
+	draw_circle(Vector2(extents.x/2, 0), 5, Color.black)
