1741639b53c129c10f5c7b6510999929ccbf3da7
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-06-27 16:35:25 -0400

    Fix audio on titlescree

--- project/src/Jukebox.gd
+++ project/src/Jukebox.gd
@@ -3,10 +3,11 @@ extends AudioStreamPlayer
 const _title_music := preload("res://assets/title.ogg")
 const _background_music := preload("res://assets/background.ogg")
 
-func _ready():
+func play_title_music():
 	stream = _title_music
 	play()
-	
+
+
 func play_background_music():
 	stream = _background_music
 	play()
--- project/src/TitleScreen.gd
+++ project/src/TitleScreen.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+func _ready():
+	Jukebox.play_title_music()
+
+
 func _input(event):
 	if event is InputEventMouseButton and event.is_pressed():
 		OS.window_fullscreen = true
