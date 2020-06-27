b9289f4b573f67a454381d84303fe3a0c3cc597f
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-06-27 09:34:35 -0400

    Add simple food flake that falls

---
+++ project/src/FoodFlake.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+
+export var fall_speed := 50
+
+onready var CollisionShape2D := $CollisionShape2D
+
+func _process(delta):
+	position += Vector2(0,fall_speed * delta)
+
+func _draw():
+	draw_circle(Vector2.ZERO, CollisionShape2D.shape.radius, Color.beige)
