c2ccd57b837a35770b221cbd9ea12bf67669b4ca
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-06-27 16:49:21 -0400

    Remove warning on changing scenes

--- project/src/TitleScreen.gd
+++ project/src/TitleScreen.gd
@@ -7,5 +7,5 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseButton and event.is_pressed():
 		OS.window_fullscreen = true
-		get_tree().change_scene("res://src/Aquarium.tscn")
+		var _error = get_tree().change_scene("res://src/Aquarium.tscn")
 		queue_free()
