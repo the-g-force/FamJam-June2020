3d4c1db32b9ac98496797d8ccb5ebb451afcab3c
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-06-27 16:03:31 -0400

    Switch to full screen when the game starts
    This is awkward on desktop but OK on mobile

--- project/src/TitleScreen.gd
+++ project/src/TitleScreen.gd
@@ -4,5 +4,6 @@ var MainScene := preload("res://src/Aquarium.tscn")
 
 func _input(event):
 	if event is InputEventMouseButton:
+		OS.window_fullscreen = true
 		get_tree().get_root().add_child(MainScene.instance())
 		queue_free()
