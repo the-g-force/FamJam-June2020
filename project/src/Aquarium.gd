fab94c0a86e5b2bc02afacb4fe3c45cba05c476e
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-06-27 10:51:48 -0400

    Add click to move fish

--- project/src/Aquarium.gd
+++ project/src/Aquarium.gd
@@ -7,4 +7,5 @@ onready var fish := $Fish
 func _process(_delta):
 	if Input.is_action_just_pressed("clicked"):
 		pointclicked = get_global_mouse_position()
-		fish.destination = pointclicked
+		if pointclicked < get_viewport_rect().size:
+			fish.destination = pointclicked
