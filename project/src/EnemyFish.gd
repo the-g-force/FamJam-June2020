01880e62426722e221dbc9a3f68c96aae34b37b2
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-06-27 12:21:49 -0400

    Add blue fish sprite that flips
    This temporarily removed any progress on the mouth and food seeking behavior,
    but we should be able to plug it back in via the destination feature.

--- project/src/EnemyFish.gd
+++ project/src/EnemyFish.gd
@@ -2,20 +2,11 @@ extends Fish
 
 export var min_x := 20
 export var max_x := 500
-export var speed := 100.0
 
 var target = null
-var _direction := Vector2(1,0)
 
-func _process(delta):
-	if target == null:
-		if get_global_transform().origin.x >= max_x:
-			_direction = Vector2(-1,0)
-		if get_global_transform().origin.x <= min_x:
-			_direction = Vector2(1,0)
-	else:
-		_direction = target.get_global_transform().origin - get_global_transform().origin
-	var _error = move_and_collide(_direction.normalized()*speed*delta)
+func _ready():
+	_set_destination(Vector2(max_x, get_global_transform().origin.y))
 
 
 func _on_Area2D_area_entered(area):
@@ -24,10 +15,10 @@ func _on_Area2D_area_entered(area):
 
 
 func _on_Mouth_area_entered(area):
-	if area is FoodFlake:
-		area.queue_free()
-		target = null
-		if get_global_transform().origin.x >= max_x:
-			_direction = Vector2(-1,0)
-		if get_global_transform().origin.x <= min_x:
-			_direction = Vector2(1,0)
+	pass
+
+
+func _on_reached_destination():
+	print('reached destination')
+	_set_destination(Vector2(min_x if destination.x == max_x else max_x, destination.y))
+	print('new destination is ', destination)
--- project/src/Fish.gd
+++ project/src/Fish.gd
@@ -1,8 +1,26 @@
 extends KinematicBody2D
 class_name Fish
 
-func _draw():
-	var extents : Vector2 = $CollisionShape2D.shape.extents
-	var r : Rect2 = Rect2(-extents.x, -extents.y, extents.x*2, extents.y*2)
-	draw_rect(r, Color.red)
-	draw_circle(Vector2(extents.x/2, 0), 5, Color.black)
+signal reached_destination
+
+const THRESHOLD := 3.0
+
+export var speed : float = 200
+var destination : Vector2 = Vector2(0,0) setget _set_destination
+var _should_move : bool = false
+
+
+func _process(delta):
+	if _should_move:
+		var direction = destination - get_global_transform().origin
+		var velocity = direction.normalized() * speed
+		$Sprite.scale.x = 1 if velocity.x > 0 else -1
+		move_and_slide(velocity)
+		if (get_global_transform().origin - destination).length_squared() < THRESHOLD:
+			_should_move = false			
+			emit_signal("reached_destination")
+	
+	
+func _set_destination(value):
+	destination = value
+	_should_move = true
